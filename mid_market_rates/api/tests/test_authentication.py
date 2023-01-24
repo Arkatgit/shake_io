2c9692f85afaec056c2f0e90e072af08f1b07013
Robert Antwi <robert@mpharma.com>
2023-01-24 03:59:05 +0000

    Added test cases for api

--- mid_market_rates/api/tests/test_authentication.py
+++ mid_market_rates/api/tests/test_authentication.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 from httpx import AsyncClient
 
@@ -13,20 +15,27 @@ class TestAuthentication:
             response = await ac.get("http://mid-market-rate:8000/history")
         assert response.status_code == 401
 
+    @pytest.mark.asyncio
     async def test_history_with_wrong_auth_key(self):
         async with AsyncClient(app=app) as ac:
             response = await ac.get(
                 "http://mid-market-rate:8000/history",
                 headers={
-                    "Authorization": "Bearer 004cec70-142d-4535-961b-4919a5d58ad3xxxxxxxx"
+                    "Authorization": "Bearer 004cec70-142d-4535-961b-4919a5d58ad3xxxxxxxx",
+                    'Content-Type': 'application/json'
                 },
             )
         assert response.status_code == 401
-
-        async def test_history_with_correct_auth_key(self):
-            async with AsyncClient(app=app) as ac:
-                response = await ac.get(
-                    "http://mid-market-rate:8000/history",
-                    headers={"Authorization": f"Bearer {settings.API_KEY}"},
-                )
-            assert response.status_code == 200
+    
+    @mock.patch(
+        "mid_market_rates.services.Database.fetch_all", side_effect=mock.AsyncMock(return_value=[])
+    )
+    @pytest.mark.asyncio
+    async def test_history_with_correct_auth_key(self, mock_save_db):
+        async with AsyncClient(app=app) as ac:
+            response = await ac.get(
+                "http://mid-market-rate:8000/history",
+                headers={"Authorization": f"Bearer {settings.API_KEY}",
+                'Content-Type': 'application/json'},
+            )
+        assert response.status_code == 200
---
+++ mid_market_rates/api/tests/test_mid_market_rate.py
@@ -0,0 +1,45 @@
+from unittest import mock
+
+import pytest
+from httpx import AsyncClient
+
+from main import app
+from mid_market_rates.config import settings
+
+
+
+class TestMidMarketRate:
+    @mock.patch(
+        "mid_market_rates.services.Database.save_many", side_effect=mock.AsyncMock(return_value=True) 
+    )
+    @mock.patch(
+        "mid_market_rates.services.Database.fetch_all", side_effect=mock.AsyncMock(return_value=[{'currency' : 'GHS'}, 
+        {'currency': "USD"}]) 
+    )
+    @mock.patch(
+        "mid_market_rates.services.Database.fetch_one", side_effect=mock.AsyncMock(return_value=[{'currency' : 'USD'}]) 
+    )
+    @pytest.mark.asyncio
+    async def test_currencies(self, one, all, many):
+        async with AsyncClient(app=app) as ac:
+            response = await ac.get("http://mid-market-rate:8000/currencies",
+                headers={"Authorization": f"Bearer {settings.API_KEY}",
+                'Content-Type': 'application/json'}
+            )
+        assert response.json() == { "items": [{"currency": "GHS"}, {"currency": "USD"}] , "total": 2, "page": 1, "size": 50}
+
+
+    @mock.patch(
+        "mid_market_rates.services.Database.fetch_all", side_effect=mock.AsyncMock(
+            return_value=[])
+    )
+    @pytest.mark.asyncio
+    async def test_history(self, mock_save_db):
+        async with AsyncClient(app=app) as ac:
+            response = await ac.get(
+                "http://mid-market-rate:8000/history",
+                headers={"Authorization": f"Bearer {settings.API_KEY}",
+                'Content-Type': 'application/json'},
+            )
+        return response.status_code == 200
+        
